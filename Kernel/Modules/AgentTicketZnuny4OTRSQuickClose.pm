6ac8522fce132ed8cf4b92062ec03df9322d8e12
Martin Edenhofer <me@edenhofer.de>
2013-01-23 18:18:43 +0100

    Init version.

---
+++ Kernel/Modules/AgentTicketZnuny4OTRSQuickClose.pm
@@ -0,0 +1,64 @@
+#
+# Copyright (C) 2013 Znuny GmbH, http://znuny.com/
+#
+
+package Kernel::Modules::AgentTicketZnuny4OTRSQuickClose;
+
+use strict;
+use warnings;
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for (qw(ParamObject DBObject LayoutObject LogObject ConfigObject QueueObject TimeObject TicketObject)) {
+        if ( !$Self->{$_} ) {
+            $Self->{LayoutObject}->FatalError( Message => "Got no $_!" );
+        }
+    }
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !$Self->{TicketID} ) {
+        return $Self->{LayoutObject}->ErrorScreen(
+            Message => 'No TicketID is given!',
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # check permissions
+    my $Access = $Self->{TicketObject}->TicketPermission(
+        Type     => 'close',
+        TicketID => $Self->{TicketID},
+        UserID   => $Self->{UserID}
+    );
+
+    # error screen, don't show ticket
+    if ( !$Access ) {
+        return $Self->{LayoutObject}->NoPermission(
+            Message    => "You need $Self->{Config}->{Permission} permissions!",
+            WithHeader => 'yes',
+        );
+    }
+
+    my $State = $Self->{ConfigObject}->Get('Znuny4OTRS::QuickClose::State');
+    if ($State) {
+        my $Success = $Self->{TicketObject}->TicketStateSet(
+            State    => $State,
+            TicketID => $Self->{TicketID},
+            UserID   => $Self->{UserID},
+        );
+    }
+    return $Self->{LayoutObject}->Redirect( OP => $Self->{LastScreenOverview} );
+}
+1;
+
---
+++ README.md
@@ -0,0 +1,31 @@
+Znuny4OTRS - Quick Close
+========================
+
+Feature List
+============
+This package functionality enables you a "Quick Close" feature in ticket action row (where you already have "note, pending, close, ...").
+
+Installation
+============
+Download the package and install it via admin interface -> package manager.
+
+Prerequisites
+* OTRS 3.1
+
+Configuration
+=============
+Note needed.
+
+Download
+========
+For download see http://znuny.com/d/
+
+Commercial Support
+==================
+For this extension and for OTRS in general visit http://znuny.com. Looking forward to hear from you!
+
+Enjoy!
+
+ Your Znuny Team!
+ http://znuny.com
+
