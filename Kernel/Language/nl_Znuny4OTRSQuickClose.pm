de1a22b18f4b2a5799df28554a82ffbe69d55bf2
sjoerddebruin <sjoerddebruin@me.com>
2016-02-12 17:01:42 +0100

    Add Dutch translation

---
+++ Kernel/Language/nl_Znuny4OTRSQuickClose.pm
@@ -0,0 +1,25 @@
+﻿# --
+# Kernel/Language/nl_Znuny4OTRSQuickClose.pm - the Dutch translation of the texts of Znuny4OTRS-QuickClose
+# Copyright (C) 2012-2016 Znuny GmbH, http://znuny.com/
+# Copyright (C) (2014) (Denny Bresch) (dennybresch@gmail.com) (https://github.com/dennybresch)
+# Copyright (C) (2016) (Sjoerd de Bruin) (https://github.com/sjoerddebruin)
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::nl_Znuny4OTRSQuickClose;
+
+use strict;
+use warnings;
+use utf8;
+
+sub Data {
+    my $Self = shift;
+
+    $Self->{Translation}->{'Quick Close'} = 'Snel sluiten';
+
+    return 1;
+}
+1;
