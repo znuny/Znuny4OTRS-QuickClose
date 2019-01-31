1c91e71d9885fdc7fdf10f621859a4e248c958f7
Denny Bresch <db@znuny.com>
2019-01-31 16:55:57 +0100

    Tidied code.

--- Kernel/Language/de_Znuny4OTRSQuickClose.pm
+++ Kernel/Language/de_Znuny4OTRSQuickClose.pm
@@ -1,6 +1,6 @@
 ﻿# --
 # Kernel/Language/de_Znuny4OTRSQuickClose.pm - the german translation of the texts of Znuny4OTRS-QuickClose
-# Copyright (C) 2012-2018 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2012-2019 Znuny GmbH, http://znuny.com/
 # Copyright (C) (2014) (Denny Bresch) (dennybresch@gmail.com) (https://github.com/dennybresch)
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
---
+++ doc/de/config.md
@@ -0,0 +1,2 @@
+# Konfiguration
+Sie können in der SysConfig der zu setzenden Status vom Schliessen-Typ einstellen. Die Grundeinstellung ist 'closed successful'
