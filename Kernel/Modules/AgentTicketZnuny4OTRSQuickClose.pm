f8f662f20cec1ef874a7d4cc09ec2de0fdd1d53d
Denny Korsukéwitz <dk@znuny.com>
2020-09-03 18:05:21 +0200

    Synchronization between otrs5 and otrs6. Special thanks goes to: @rsnakin (https://github.com/rsnakin)

--- Kernel/Modules/AgentTicketZnuny4OTRSQuickClose.pm
+++ Kernel/Modules/AgentTicketZnuny4OTRSQuickClose.pm
@@ -1,6 +1,8 @@
 # --
 # Copyright (C) 2012-2020 Znuny GmbH, http://znuny.com/
-# Copyright (C) (2014) (Denny Bresch) (dennybresch@gmail.com) (https://github.com/dennybresch)
+# Copyright (C) (2014) (Denny Korsukéwitz) (dennykorsukewitz@gmail.com) (https://github.com/dennykorsukewitz)
+# Copyright (C) (2020) (rsnakin) (https://github.com/rsnakin)
+
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -9,20 +11,24 @@
 
 package Kernel::Modules::AgentTicketZnuny4OTRSQuickClose;
 
+use Kernel::System::VariableCheck qw(:all);
+
 use strict;
 use warnings;
 
 our @ObjectDependencies = (
     'Kernel::Config',
     'Kernel::Output::HTML::Layout',
+    'Kernel::System::DynamicField',
+    'Kernel::System::DynamicField::Backend',
     'Kernel::System::Ticket',
     'Kernel::System::Ticket::Article',
+    'Kernel::System::Web::Request',
 );
 
 sub new {
     my ( $Type, %Param ) = @_;
 
-    # allocate new hash for object
     my $Self = {%Param};
     bless( $Self, $Type );
 
@@ -32,12 +38,21 @@ sub new {
 sub Run {
     my ( $Self, %Param ) = @_;
 
-    my $LayoutObject  = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
-    my $TicketObject  = $Kernel::OM->Get('Kernel::System::Ticket');
-    my $ArticleObject = $Kernel::OM->Get('Kernel::System::Ticket::Article');
-    my $ConfigObject  = $Kernel::OM->Get('Kernel::Config');
+    my $LayoutObject              = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $TicketObject              = $Kernel::OM->Get('Kernel::System::Ticket');
+    my $ArticleObject             = $Kernel::OM->Get('Kernel::System::Ticket::Article');
+    my $ConfigObject              = $Kernel::OM->Get('Kernel::Config');
+    my $ParamObject               = $Kernel::OM->Get('Kernel::System::Web::Request');
+    my $DynamicFieldObject        = $Kernel::OM->Get('Kernel::System::DynamicField');
+    my $DynamicFieldBackendObject = $Kernel::OM->Get('Kernel::System::DynamicField::Backend');
 
-    # check needed stuff
+    my @ParamNames = $ParamObject->GetParamNames();
+
+    my %GetParam;
+    for my $Param (@ParamNames) {
+        $GetParam{$Param} = $ParamObject->GetParam( Param => $Param ) || '';
+    }
+
     if ( !$Self->{TicketID} ) {
         return $LayoutObject->ErrorScreen(
             Message => 'No TicketID is given!',
@@ -45,14 +60,12 @@ sub Run {
         );
     }
 
-    # check permissions
     my $Access = $TicketObject->TicketPermission(
         Type     => 'close',
         TicketID => $Self->{TicketID},
         UserID   => $Self->{UserID}
     );
 
-    # error screen, don't show ticket
     if ( !$Access ) {
         return $LayoutObject->NoPermission(
             Message    => "You need $Self->{Config}->{Permission} permissions!",
@@ -60,9 +73,50 @@ sub Run {
         );
     }
 
-    $Self->_SetState();
-
     my $Config = $ConfigObject->Get('Znuny4OTRSQuickClose');
+
+    my $DynamicField = $DynamicFieldObject->DynamicFieldListGet(
+        Valid      => 1,
+        ObjectType => 'Ticket',
+    );
+
+    GETPARAM:
+    for my $Param ( sort keys %GetParam ) {
+
+        # set dynamic fields values via url / link (DynamicField_NAME=ZnunyRocks;)
+        next GETPARAM if $Param !~ m{^DynamicField_(.*)}xms;
+        if ( $Param =~ m{^DynamicField_(.*)}xms ) {
+
+            my $Value              = $GetParam{$Param} || '';
+            my $DynamicFieldName   = $1;
+            my $DynamicFieldConfig = ( grep { $_->{Name} eq $DynamicFieldName } @{$DynamicField} )[0];
+
+            next GETPARAM if !defined $Value || !IsHashRefWithData($DynamicFieldConfig);
+
+            $DynamicFieldBackendObject->ValueSet(
+                DynamicFieldConfig => $DynamicFieldConfig,
+                ObjectID           => $Self->{TicketID},
+                Value              => $Value,
+                UserID             => $Self->{UserID},
+            );
+        }
+    }
+
+    my $State = $GetParam{State} || $ConfigObject->Get('Znuny4OTRS::QuickClose::State');
+    if ($State) {
+        my $Success = $TicketObject->TicketStateSet(
+            State    => $State,
+            TicketID => $Self->{TicketID},
+            UserID   => $Self->{UserID},
+        );
+        if ($Success) {
+            $TicketObject->TicketLockSet(
+                TicketID => $Self->{TicketID},
+                Lock     => 'unlock',
+                UserID   => $Self->{UserID},
+            );
+        }
+    }
 
     return $LayoutObject->Redirect( OP => $Self->{LastScreenOverview} ) if !$Config->{Article};
 
@@ -81,33 +135,6 @@ sub Run {
     );
 
     return $LayoutObject->Redirect( OP => $Self->{LastScreenOverview} );
-}
-
-sub _SetState {
-    my ( $Self, %Param ) = @_;
-
-    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
-    my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
-
-    my $State = $ConfigObject->Get('Znuny4OTRS::QuickClose::State');
-
-    return if !$State;
-
-    my $Success = $TicketObject->TicketStateSet(
-        State    => $State,
-        TicketID => $Self->{TicketID},
-        UserID   => $Self->{UserID},
-    );
-
-    return if !$Success;
-
-    $TicketObject->TicketLockSet(
-        TicketID => $Self->{TicketID},
-        Lock     => 'unlock',
-        UserID   => $Self->{UserID},
-    );
-
-    return 1;
 }
 
 1;
--- doc/de/config.md
+++ doc/de/config.md
@@ -2,7 +2,6 @@
 
 Sie können in der SysConfig folgende Einstellungen am Ticket durch Nutzung der Quick-Close-Funktion konfigurieren:
 
-
 ## Article
 `Znuny4OTRSQuickClose###Article`
 
@@ -45,3 +44,40 @@ Definiert den History-Kommentar für die Schnellschließaktion, der für die Tic
 
 ## Znuny4OTRS::QuickClose::State
 Definiert den Status, der am Ticket durch die Quick-Close Funktion gespeichert wird. Die Grundeinstellung ist 'closed successful'.
+
+## Dynamische Felder
+
+In den folgenden SysConfigs, kann der Wert 'Link' so erweitert werden, dass dynamische Felder direkt per Klick befüllt werden.
+
+- Ticket::Frontend::MenuModule###451-QuickClose
+- Ticket::Frontend::MenuModule###452-QuickClose
+- Ticket::Frontend::MenuModule###453-QuickClose
+- Ticket::Frontend::MenuModule###454-QuickClose
+- Ticket::Frontend::MenuModule###455-QuickClose
+- Ticket::Frontend::PreMenuModule###441-QuickClose
+- Ticket::Frontend::PreMenuModule###442-QuickClose
+- Ticket::Frontend::PreMenuModule###443-QuickClose
+- Ticket::Frontend::PreMenuModule###444-QuickClose
+- Ticket::Frontend::PreMenuModule###445-QuickClose
+
+Beispiel:
+
+Der folgende Link setzt das dynamische Feld 'Znuny1' auf 'leer' und das dynamische Feld 'Znuny2' auf '2'.
+
+```
+Action=AgentTicketZnuny4OTRSQuickClose;TicketID=[% Data.TicketID | html %];DynamicField_Znuny1=;DynamicField_Znuny2=2;
+```
+
+Weitere Beispiele:
+
+- DynamicField_Checkbox=1;
+- DynamicField_Date=2020-02-20;
+- DynamicField_DateTime=2020-02-20 12:25:08;
+- DynamicField_Dropdown=Key 1;
+- DynamicField_Multiselect=Key 1;
+- DynamicField_Text=SomeText;
+- DynamicField_Textarea=SomeText;
+
+```
+Action=AgentTicketZnuny4OTRSQuickClose;TicketID=[% Data.TicketID | html %];DynamicField_Checkbox=1;DynamicField_Date=2020-02-20;DynamicField_DateTime=2020-02-20 12:25:08;DynamicField_Dropdown=Key 1;DynamicField_Multiselect=Key 1;DynamicField_Text=SomeText;DynamicField_Textarea=SomeText;
+```
