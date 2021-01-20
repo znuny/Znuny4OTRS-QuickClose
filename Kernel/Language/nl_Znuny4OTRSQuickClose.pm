98230606b840240889afdef1eaffb2c641f93be3
Denny Korsukéwitz <dk@znuny.com>
2021-01-20 13:31:22 +0100

    Happy new year. Tidied code.

--- Kernel/Language/nl_Znuny4OTRSQuickClose.pm
+++ Kernel/Language/nl_Znuny4OTRSQuickClose.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2012-2020 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2012-2021 Znuny GmbH, http://znuny.com/
 # Copyright (C) (2016) (Sjoerd de Bruin) (https://github.com/sjoerddebruin)
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
