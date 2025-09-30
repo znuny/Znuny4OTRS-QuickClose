76827358646887c33eda63170419369dcbf3bf4c
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2025-09-30 09:00:46 +0200

    Applied CodePolicy.

--- var/packagesetup/ZnunyQuickClose.pm
+++ var/packagesetup/ZnunyQuickClose.pm
@@ -200,7 +200,7 @@ sub _MigrateSysConfigSettings {
 
             $LogObject->Log(
                 Priority => 'error',
-                Message =>
+                Message  =>
                     "Error: Unable to migrate value of SysConfig option $OriginalSysConfigOptionName to option $NewSysConfigOptionName",
             );
         }
